cdce603b1df597a131ffdaafb67b6b63c597cadd
Felipe Lacerda <felipe.lacerda2@fatec.sp.gov.br>
2024-07-30 02:07:37 -0300

    0.5

--- src/main/java/com/to_do/to_do_list/ToDoListApplication.java
+++ src/main/java/com/to_do/to_do_list/ToDoListApplication.java
@@ -4,9 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-@SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "To-Do List API", version = "1.0", description = "API para gerenciamento de tarefas"))
+import io.swagger.v3.oas.annotations.servers.Server;
 
+@SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "To-Do List API", version = "1.0", description = "API para gerenciamento de tarefas"),
+		servers = {
+				@Server(url = "https://api-to-do-list-api-web.up.railway.app", description = "URL do servidor de produção")
+		}
+)
 public class ToDoListApplication {
 
 	public static void main(String[] args) {
